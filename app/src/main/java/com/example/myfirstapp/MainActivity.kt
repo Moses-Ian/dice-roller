7c482219349fd943514edfba9d929020b39a6a2a
Expired <imoses2@hotmail.com>
2022-07-18 00:15:43 -0700

    button rolls a dice

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -12,9 +12,23 @@ class MainActivity : AppCompatActivity() {
     setContentView(R.layout.activity_main)
 
 		val rollButton: Button = findViewById(R.id.button)
-		rollButton.setOnClickListener {
-			val resultTextView: TextView = findViewById(R.id.textView)
-			resultTextView.text = "6"
-		}
+		rollButton.setOnClickListener { rollDice() }
+  }
+	
+	private fun rollDice() {
+		// Creat a Dice with 6 sides and roll it
+		val dice = Dice(6)
+		val diceRoll = dice.roll()
+		
+		// Update the screen with the result
+		val resultTextView: TextView = findViewById(R.id.textView)
+		resultTextView.text = diceRoll.toString()
+	}
+}
+
+class Dice(private val numSides: Int) {
+
+  fun roll(): Int {
+    return (1..numSides).random()
   }
 }
