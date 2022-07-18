1a6175d61b548aeec272443f931bbf933cec823f
Expired <imoses2@hotmail.com>
2022-07-18 00:53:15 -0700

    added dice images

---
+++ README.md
@@ -0,0 +1,13 @@
+# Dice Roller
+
+## Description
+
+This is a learning project. I'm following the course at
+
+https://developer.android.com/courses/pathways/android-basics-kotlin-four#codelab-https://developer.android.com/codelabs/basic-android-kotlin-training-create-dice-roller-app-with-button
+
+This is really just an opportunity for me to learn Android development and Kotlin
+
+## Credits
+
+Created by Ian Moses
--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.myfirstapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -13,16 +13,29 @@ class MainActivity : AppCompatActivity() {
 
 		val rollButton: Button = findViewById(R.id.button)
 		rollButton.setOnClickListener { rollDice() }
+		
+		//do a dice roll when the app starts
+		rollDice()
   }
 	
 	private fun rollDice() {
-		// Creat a Dice with 6 sides and roll it
+		// Create a Dice with 6 sides and roll it
 		val dice = Dice(6)
 		val diceRoll = dice.roll()
 		
 		// Update the screen with the result
-		val resultTextView: TextView = findViewById(R.id.textView)
-		resultTextView.text = diceRoll.toString()
+		val diceImage: ImageView = findViewById(R.id.imageView)
+		val drawableResource = when (diceRoll) {
+			1 -> R.drawable.dice_1
+			2 -> R.drawable.dice_2
+			3 -> R.drawable.dice_3
+			4 -> R.drawable.dice_4
+			5 -> R.drawable.dice_5
+			else -> R.drawable.dice_6
+		}
+		diceImage.setImageResource(drawableResource)
+		// screen readers
+		diceImage.contentDescription = diceRoll.toString()
 	}
 }
 
